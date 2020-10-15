0fdfd631faa63fa01234bac79baf6074cbecd53a
John Doe <dhouib@rhrk.uni-kl.de>
2020-10-15 08:25:48 +0200

    inital version , next : Layout -> buttons

--- src/app/custom-nodes/custom-nodes.component.ts
+++ src/app/custom-nodes/custom-nodes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Edge, Node, Network, DataSet } from 'vis-network/standalone';
+import anything = jasmine.anything;
+import set = Reflect.set;
 
 @Component({
   selector: 'app-custom-nodes',
@@ -38,15 +40,19 @@ export class CustomNodesComponent implements OnInit {
   }
 
   link() {
-    if (this.on) {
-    this.nodes.update([{font : {color: 'rgb(159, 249, 0)'} }]);
-    }
-    else {
-      this.nodes.update([{ font : {color: 'black'} }]);
-
-    }
     this.edges.add({from: 1, to: 2});
 
+  }
+  toggle_labels() {
+    this.nodes.getIds();
+    if (this.on) {
+      for ( const id of this.nodes.getIds()) {
+        this.nodes.update([{id: id , font: {color: 'rgb(159, 249, 0)'} }]);
+      }
+    } else {
+      this.nodes.update([{id: 1 , font : {color: 'black'} }]);
+      this.on = true;
+    }
   }
 
 }
