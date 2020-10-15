d162afff84aef2865d32f4164abf30cccc0e1460
John Doe <dhouib@rhrk.uni-kl.de>
2020-10-15 21:29:45 +0200

    rami dhouib adding comments

--- src/app/custom-nodes/custom-nodes.component.ts
+++ src/app/custom-nodes/custom-nodes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Edge, Node, Network, DataSet } from 'vis-network/standalone';
-import set = Reflect.set;
 
 @Component({
   selector: 'app-custom-nodes',
@@ -8,14 +7,19 @@ import set = Reflect.set;
   styleUrls: ['./custom-nodes.component.css']
 })
 export class CustomNodesComponent implements OnInit {
-
+  //  Network contains all the view( nodes and edges and other information )
   network: Network;
   nodes: DataSet<Node>;
   edges: DataSet<Edge>;
+  // contains if label is on or off
   on = true ;
+  // contains next node
   i = 2;
+  // contains user input as number
   node1: number;
   node2: number;
+  // functions that store the input
+  // see html code
   update1(value) {
     this.node1 = value;
   }
@@ -24,6 +28,7 @@ export class CustomNodesComponent implements OnInit {
   }
 
   constructor() {
+    // Adding  Node1
     this.nodes = new DataSet([
       {
         id: 1, label: 'Node 1', color: {
@@ -40,7 +45,7 @@ export class CustomNodesComponent implements OnInit {
     const data = { nodes: this.nodes, edges: this.edges };
     this.network = new Network(container, data, options);
   }
-
+  // adds a node
   add() {
     this.nodes.add({ id: this.i, label: 'Node' + this.i , color: {
         background: 'rgb(159, 249, 0)',
@@ -48,26 +53,29 @@ export class CustomNodesComponent implements OnInit {
       }});
     this.i++;
   }
-
+  // links 2 nodes
   link() {
+    // testing if user input is stored correctly
     console.log(this.node1);
     console.log(this.node2);
-    this.edges.add({from: this.node1, to: this.node2 , color : 'blue' });
+    // adding the edge
+    this.edges.update({from: this.node1, to: this.node2 , color : 'blue' });
 
   }
+  // toggles label
   toggle_labels() {
-    this.nodes.getIds();
+    // if label is on
     if (this.on) {
       for (let j = 1; j < this.i ; j++) {
-        this.nodes.update([{id: j , font: {color: 'rgb(159, 249, 0)'} }]);
+        this.nodes.add([{id: j , font: {color: 'rgb(159, 249, 0)'} }]);
         this.on = false;
 
       }
+      // label is off
     } else {
       for (let j = 1; j < this.i ; j++) {
         this.nodes.update([{id: j , font: {color: 'black'} }]);
         this.on = true;
     }
   }
-
 }}
