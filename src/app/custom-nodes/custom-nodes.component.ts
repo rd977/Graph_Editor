339ccccb6a3514333ed34ab0a79bbc44877f64c2
John Doe <dhouib@rhrk.uni-kl.de>
2020-10-15 08:01:10 +0200

    inital version , next : Layout -> buttons

--- src/app/custom-nodes/custom-nodes.component.ts
+++ src/app/custom-nodes/custom-nodes.component.ts
@@ -11,21 +11,20 @@ export class CustomNodesComponent implements OnInit {
   network: Network;
   nodes: DataSet<Node>;
   edges: DataSet<Edge>;
-  isAdded: boolean;
+  on = true ;
+  i = 2;
 
   constructor() {
     this.nodes = new DataSet([
-      { id: 1, label: 'Node 1' },
-      { id: 2, label: 'Node 2' },
-      { id: 3, label: 'Node 3' },
-      { id: 4, label: 'Node 4' },
-      { id: 5, label: 'Node 5' },
+      {
+        id: 1, label: 'Node 1', color: {
+          background: 'rgb(159, 249, 0)',
+          border: 'black',
+        }
+      }
     ]);
-    this.edges = new DataSet([
-      {from: 1,to: 2}
-    ]);
-  }
-
+    this.edges = new DataSet([]);
+    }
   ngOnInit() {
     const options = {};
     const container = document.getElementById('mynetwork-vis');
@@ -34,9 +33,20 @@ export class CustomNodesComponent implements OnInit {
   }
 
   add() {
+    this.nodes.add({ id: this.i, label: 'Node' + this.i });
+    this.i++;
   }
 
   link() {
+    if (this.on) {
+    this.nodes.update([{font : {color: 'rgb(159, 249, 0)'} }]);
+    }
+    else {
+      this.nodes.update([{ font : {color: 'black'} }]);
+
+    }
+    this.edges.add({from: 1, to: 2});
+
   }
 
 }
